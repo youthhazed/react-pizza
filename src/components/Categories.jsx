86b87f0cec48aecb7fd3d695d1ea04ed88478fa6
youthhazed <term7ina3tor@gmail.com>
2024-08-05 15:50:36 +0300

    html/css, components started

---
+++ src/components/Categories.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+
+
+function Categories() {
+
+    const [activeIndex, setActiveIndex] = React.useState(0);
+
+    function onClickCategory(index) {
+        setActiveIndex(index);
+    }
+
+    const categories = [
+        'Все',
+        'Мясные',
+        'Вегетарианская',
+        'Гриль',
+        'Острые',
+        'Закрытые'
+    ]
+
+    return (
+      <div className="categories">
+        <ul>
+            {
+            categories.map((value, i) =>(
+                <li onClick={() => onClickCategory(i)} className={activeIndex === i ? 'active' : ''}>{value}</li> 
+            ))
+            }
+        </ul>
+      </div>
+    );
+  }
+export default Categories;  
